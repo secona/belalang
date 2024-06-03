12fe231e9af20b5f84ed78f5bb29c7cb5697510b
secona <77039267+secona@users.noreply.github.com>
2024-06-03 21:55:13 +0700

    feat: CLI file runner

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,11 +1,53 @@
+use std::error::Error;
+use std::fs;
+use std::path::PathBuf;
+
 use belalang::evaluator::Evaluator;
 use belalang::lexer::Lexer;
-use belalang::parser::Parser;
+use belalang::parser::Parser as BelalangParser;
+use clap::Parser;
 use rustyline::error::ReadlineError;
 use rustyline::DefaultEditor;
-use rustyline::Result;
 
-fn main() -> Result<()> {
+#[derive(clap::Parser)]
+struct CLI {
+    filename: Option<PathBuf>,
+}
+
+fn main() -> Result<(), ()> {
+    let cli = CLI::parse();
+
+    let _ = match cli.filename {
+        Some(filename) => run_file(filename),
+        None => repl(),
+    };
+
+    Ok(())
+}
+
+fn run_file(filename: PathBuf) -> Result<(), Box<dyn Error>> {
+    let file = fs::read(filename).expect("Unable to read file!");
+
+    let lexer = Lexer::new(file.into());
+    let mut parser = BelalangParser::new(lexer);
+
+    match parser.parse_program() {
+        Ok(program) => {
+            let mut ev = Evaluator::new(program);
+            println!("{:?}", ev.evaluate());
+        }
+        Err(errors) => {
+            println!("parser errors:");
+            for error in errors {
+                println!("- {}", error);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn repl() -> Result<(), Box<dyn Error>> {
     println!("Welcome to Belalang REPL v{}!\n", env!("CARGO_PKG_VERSION"));
 
     let mut rl = DefaultEditor::new()?;
@@ -17,7 +59,7 @@ fn main() -> Result<()> {
                 let _ = rl.add_history_entry(line.as_str());
 
                 let lexer = Lexer::new(line.as_bytes().into());
-                let mut parser = Parser::new(lexer);
+                let mut parser = BelalangParser::new(lexer);
 
                 match parser.parse_program() {
                     Ok(program) => match ev.evaluate_statements(program.statements) {
