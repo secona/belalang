4655010576852f39d0fcfdf54675c7ace88c9c9c
secona <77039267+secona@users.noreply.github.com>
2024-06-11 11:01:06 +0700

    feat: improve UI

--- web/src/main.ts
+++ web/src/main.ts
@@ -7,14 +7,50 @@ import { bracketMatching, indentOnInput } from '@codemirror/language';
 import { EditorView, highlightActiveLine, highlightActiveLineGutter, keymap, lineNumbers } from '@codemirror/view';
 import { EditorState } from '@codemirror/state';
 
-console.log(rs);
-
-function runCode(view: EditorView) {
-  rs.run_code(view.state.doc.toString());
-  return true;
+function clamp(min: number, value: number, max: number): number {
+  return Math.max(min, Math.min(value, max));
 }
 
-new EditorView({
+const handle = document.getElementById("handle")!;
+const editor = document.getElementById("editor")!;
+const out = document.getElementById("out-wrapper")!;
+
+let dragging = false;
+
+handle.addEventListener("mousedown", _ => {
+  dragging = true;
+});
+
+document.addEventListener("mousemove", e => {
+  if (dragging) {
+    const editorWidth = clamp(20, (e.clientX / window.innerWidth) * 100, 80);
+    const outWidth = 100 - editorWidth;
+
+    editor.style.width = `${editorWidth}%`;
+    out.style.width = `${outWidth}%`;
+  }
+});
+
+document.addEventListener("mouseup", _ => {
+  dragging = false;
+})
+
+function runCode() {
+  rs.run_code(editorView.state.doc.toString());
+  return true;
+}
+(window as any).runCode = runCode;
+
+function clearConsole() {
+  const out = document.getElementById("out")!;
+
+  while (out.firstChild) {
+    out.removeChild(out.lastChild!);
+  }
+}
+(window as any).clearConsole = clearConsole;
+
+const editorView = new EditorView({
   parent: document.getElementById("editor")!,
   state: EditorState.create({
     doc: 'println("hello, world");',
