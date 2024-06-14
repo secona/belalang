e8cfea113495c9b7444823c53fb991cfaf3e16f7
secona <77039267+secona@users.noreply.github.com>
2024-06-14 23:07:56 +0700

    feat: improve examples for web

--- web/src/main.ts
+++ web/src/main.ts
@@ -67,10 +67,121 @@ function exampleSelector() {
 }
 (window as any).exampleSelector = exampleSelector;
 
+const examples: Record<string, string> = {
+  "Hello World": `println("Hello, World!");`,
+
+  "Variables": `# Declare variables using the \`:=\` operator
+# Currently, Belalang only supports integers and strings
+int_var := 123;
+str_var := "Hello, World!";
+
+# Print variables to the console using the builtin \`println\` function
+println("the value of int_var is", int_var);
+println("the value of str_var is", str_var);
+
+# Assign new values to existing variables using the \`=\` operator
+int_var = 1;
+str_var = "Hello, Mom!";
+
+# Print the new values of the variables
+println("the new value of int_var is", int_var);
+println("the new value of str_var is", str_var);`,
+
+  "If-Else If-Else": `# Variable declaration and assignment
+price := 50;
+
+# Conditional statement example
+if (price < 30) {
+    println("very cheap");
+} else if (price < 40) {
+    println("not so cheap");
+} else {
+    println("very not cheap");
+}
+
+# Using if as an expression
+status := if (price < 30) {
+    "very cheap"
+} else if (price < 40) {
+    "not so cheap"
+} else {
+    "very not cheap"
+};
+
+# Print the status
+println(status);`,
+
+  "Functions": `# Functions are declared like variables using the \`fn\` keyword
+# Define a function to add two numbers
+add := fn(x, y) {
+    return x + y;
+};
+
+# Call the add function and print the result
+println("1 + 2 =", add(1, 2));
+
+# You can also take advantage of expressions to return values
+# Define a function to multiply two numbers using an expression
+mul := fn(x, y) { x * y };
+
+# Call the mul function and print the result
+println("3 * 4 =", mul(3, 4));`,
+
+  "While Loops": `# While loops are declared with the \`while\` keyword.
+i := 0;
+while (i < 10) {
+  println("i =", i);
+  i = i + 1;
+}`,
+
+  "Closures": `# Closures!
+adder := fn() {
+  sum := 0;
+  return fn(n) {
+    sum = sum + n;
+    return sum;
+  };
+};
+
+# Each instance of adder has different
+# values of sum
+f := adder();
+g := adder();
+
+x := 1;
+while (x < 10) {
+  println("f =", f(1), "| g =", g(2));
+  x = x + 1;
+}`,
+
+  "Factorial": `# Recursions!
+fact := fn(n) {
+  # The base case
+  if (n < 2) {
+    return 1;
+  }
+  
+  # The recursive case
+  return n * fact(n - 1);
+};
+
+println("5! =", fact(5));`
+}
+
+const exampleSelectorEl = document.getElementById("example-selector")! as HTMLSelectElement;
+
+for (const key in examples) {
+  const option = document.createElement("option");
+  option.value = key;
+  option.append(key);
+
+  exampleSelectorEl.appendChild(option);
+}
+
 const editorView = new EditorView({
   parent: document.getElementById("editor")!,
   state: EditorState.create({
-    doc: 'println("hello, world");',
+    doc: examples["Hello World"],
     extensions: [
       bracketMatching(),
       closeBrackets(),
@@ -89,63 +200,3 @@ const editorView = new EditorView({
     ],
   }),
 });
-
-const examples: Record<string, string> = {
-  "Variable Declaration": `x := 10;
-println("the value of x is", x);`,
-
-  "Variable Assignment": `x := 10;
-println("the value of x is", x);
-
-x = 5;
-println("the value of x is", x);`,
-
-  "If-else": `if (true) {
-  println("Hello, World");
-} else {
-  println("dlroW, olleH");
-};`,
-
-  "Functions": `add := fn(x, y) {
-  return x + y;
-};
-
-println("1 + 2 =", add(1, 2));`,
-
-  "Closures": `adder := fn() {
-  sum := 0;
-  return fn(n) {
-    sum = sum + n;
-    return sum;
-  };
-};
-
-f := adder();
-g := adder();
-
-x := 1;
-while (x < 10) {
-  println("f =", f(1), "| g =", g(2));
-  x = x + 1;
-}`,
-
-  "Factorial": `fact := fn(n) {
-  if (n < 2) {
-    return 1;
-  } else {
-    return n * fact(n - 1);
-  };
-};
-
-println(fact(5));`
-}
-
-const exampleSelectorEl = document.getElementById("example-selector")! as HTMLSelectElement;
-
-for (const key in examples) {
-  const option = document.createElement("option");
-  option.value = key;
-  option.append(key);
-
-  exampleSelectorEl.appendChild(option);
-}
