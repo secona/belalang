949b5f8c282cd9c4c6a030574467dd432982b253
secona <77039267+secona@users.noreply.github.com>
2024-08-16 08:12:26 +0700

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Belalang Project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        rust-analyzer
+        clippy
+        rustc
+        cargo
+      ];
+    };
+  };
+}
