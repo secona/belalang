76ac15e1d094c588785e55d14f8d709eb8eb4875
secona <77039267+secona@users.noreply.github.com>
2024-10-22 09:41:24 +0700

    chore: flake.nix

--- flake.nix
+++ flake.nix
@@ -1,21 +1,40 @@
 {
-  description = "Belalang Project";
+  description = "The Belalang Programming Language";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        rust-analyzer
-        clippy
-        rustc
-        cargo
-      ];
-    };
-  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      belalang = pkgs.rustPlatform.buildRustPackage {
+        pname = "belalang";
+        version = "0.1.0";
+        cargoLock.lockFile = ./Cargo.lock;
+        src = pkgs.lib.cleanSource ./.;
+      };
+    in {
+      packages.default = belalang;
+
+      devShells.default = pkgs.mkShell {
+        name = "belalang";
+
+        inputsFrom = [belalang];
+
+        buildInputs = with pkgs; [
+          rust-analyzer
+          rustfmt
+          clippy
+        ];
+      };
+    });
 }
