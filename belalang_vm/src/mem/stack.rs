77e3f2757a56cde476cafdf56fdec7bad7760152
secona <77039267+secona@users.noreply.github.com>
2025-01-06 14:23:01 +0700

    feat(vm): implement stack frames

--- belalang_vm/src/mem/stack.rs
+++ belalang_vm/src/mem/stack.rs
@@ -4,6 +4,7 @@ pub struct StackAllocator {
     stack: [u8; STACK_SIZE],
     cap: usize,
     sp: usize,
+    fp: usize,
 }
 
 impl Default for StackAllocator {
@@ -18,6 +19,7 @@ impl StackAllocator {
             stack: [0; STACK_SIZE],
             cap: STACK_SIZE,
             sp: 0,
+            fp: 0,
         }
     }
 
@@ -46,6 +48,22 @@ impl StackAllocator {
             Some(self.stack[self.sp - 1])
         }
     }
+
+    pub fn push_frame(&mut self, locals_count: u8, return_address: u8) {
+        self.push(return_address);
+        self.push(self.fp as u8);
+        self.fp = self.sp;
+
+        for _ in 0..locals_count {
+            self.push(0);
+        }
+    }
+
+    pub fn pop_frame(&mut self) -> Option<u8> {
+        self.sp = self.fp;
+        self.fp = self.pop()? as usize;
+        self.pop()
+    }
 }
 
 #[cfg(test)]
@@ -73,5 +91,34 @@ mod tests {
         assert_eq!(allocator.pop(), Some(11));
         assert_eq!(allocator.pop(), Some(10));
         assert_eq!(allocator.pop(), None);
+    }
+
+    #[test]
+    fn allocator_push_frame() {
+        let mut allocator = StackAllocator::new();
+
+        allocator.push_frame(3, 12);
+
+        assert_eq!(allocator.fp, 2);
+        assert_eq!(allocator.sp, 5);
+
+        assert_eq!(allocator.pop(), Some(0)); // local 1
+        assert_eq!(allocator.pop(), Some(0)); // local 2
+        assert_eq!(allocator.pop(), Some(0)); // local 3
+
+        assert_eq!(allocator.pop(), Some(0)); // fp
+
+        assert_eq!(allocator.pop(), Some(12)); // return address
+    }
+
+    #[test]
+    fn allocator_pop_frame() {
+        let mut allocator = StackAllocator::new();
+
+        allocator.push_frame(3, 12);
+        allocator.pop_frame();
+
+        assert_eq!(allocator.sp, 0);
+        assert_eq!(allocator.fp, 0);
     }
 }
