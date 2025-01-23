a7865f24cc0c2ff38e9732d28a3ca423e77b07bb
secona <77039267+secona@users.noreply.github.com>
2025-01-23 16:07:23 +0700

    chore(nix): use rust-overlay and naersk

--- flake.nix
+++ flake.nix
@@ -4,37 +4,51 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    rust-overlay,
+    naersk,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [ (import rust-overlay) ];
+
       pkgs = import nixpkgs {
-        inherit system;
+        inherit system overlays;
       };
 
-      belalang = pkgs.rustPlatform.buildRustPackage {
-        pname = "belalang";
+      toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+      naersk' = pkgs.callPackage naersk {
+        cargo = toolchain;
+        rustc = toolchain;
+        clippy = toolchain;
+      };
+
+      belalang = naersk'.buildPackage {
+        name = "belalang";
         version = "0.1.0";
-        cargoLock.lockFile = ./Cargo.lock;
-        src = pkgs.lib.cleanSource ./.;
+        src = ./.;
       };
     in {
       packages.default = belalang;
 
       devShells.default = pkgs.mkShell {
         name = "belalang";
-
-        inputsFrom = [belalang];
-
-        buildInputs = with pkgs; [
-          rust-analyzer
-          rustfmt
-          clippy
-        ];
+        buildInputs = [toolchain];
       };
     });
 }
