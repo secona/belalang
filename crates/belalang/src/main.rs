e56819829c45c1be0629a2c4cc6ca04fc84c559f
secona <77039267+secona@users.noreply.github.com>
2025-03-12 11:53:40 +0700

    feat(cli): use clap build API

--- crates/belalang/src/main.rs
+++ crates/belalang/src/main.rs
@@ -1,21 +1,72 @@
 #![allow(clippy::upper_case_acronyms)]
 
 use belalang::execute_file;
-use clap::Parser;
-use std::error::Error;
-use std::path::PathBuf;
+use clap::{Arg, Command};
 
-#[derive(clap::Parser)]
-struct CLI {
-    filename: PathBuf,
-}
+fn main() {
+    let matches = Command::new("belalang")
+        .version("1.0")
+        .about("Belalang CLI")
+        .arg(
+            Arg::new("filename")
+                .help("Directly run a Belalang source code (.bel) (if no subcommand is given)")
+                .index(1),
+        )
+        .subcommand(
+            Command::new("build")
+                .about("Build a Belalang project")
+                .alias("b")
+                .arg(Arg::new("directory").required(true).index(1)),
+        )
+        .subcommand(
+            Command::new("run")
+                .about("Run a Belalang project")
+                .alias("r")
+                .arg(Arg::new("directory").required(true).index(1)),
+        )
+        .subcommand(
+            Command::new("compile")
+                .about("Compile a Belalang source code (.bel) to Belalang byte code (.belc)")
+                .alias("c")
+                .arg(Arg::new("filename").required(true).index(1)),
+        )
+        .subcommand(
+            Command::new("exec")
+                .about("Execute a Belalang byte code (.belc)")
+                .alias("x")
+                .arg(Arg::new("filename").required(true).index(1)),
+        )
+        .get_matches();
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let cli = CLI::parse();
-
-    if let Err(err) = execute_file(cli.filename) {
-        eprintln!("{}", err);
+    match matches.subcommand() {
+        Some(("build", sub_m)) => {
+            let directory = sub_m.get_one::<String>("directory").unwrap();
+            println!("Building: {}", directory);
+        }
+        Some(("run", sub_m)) => {
+            let directory = sub_m.get_one::<String>("directory").unwrap();
+            println!("Running: {}", directory);
+        }
+        Some(("compile", sub_m)) => {
+            let filename = sub_m.get_one::<String>("filename").unwrap();
+            println!("Compiling: {}", filename);
+        }
+        Some(("exec", sub_m)) => {
+            let filename = sub_m.get_one::<String>("filename").unwrap();
+            println!("Executing: {}", filename);
+        }
+        Some(("completions", sub_m)) => {
+            let shell = sub_m.get_one::<String>("shell").unwrap();
+            println!("Generating completions for: {}", shell);
+        }
+        None => {
+            if let Some(filename) = matches.get_one::<String>("filename") {
+                execute_file(filename.into()).unwrap();
+            } else {
+                println!("Running REPL");
+            }
+        }
+        _ => unreachable!(),
     }
-
-    Ok(())
 }
+
