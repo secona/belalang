b82ae561e101ac11e7c53e9f5f71f9929ebe9cfd
secona <77039267+secona@users.noreply.github.com>
2025-04-30 05:55:26 +0700

    chore: move belalang to src

--- crates/belalang/tests/examples.rs
+++
@@ -1,9 +0,0 @@
-// use std::fs;
-//
-// #[test]
-// fn examples_work() {
-//     for filename in fs::read_dir("../examples").unwrap() {
-//         let filename = filename.unwrap().path();
-//         belalang::run(filename).unwrap();
-//     }
-// }
